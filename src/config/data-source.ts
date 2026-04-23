d21d22ca8c506c05044516391c6b6b2a5927dda0
Lucas Fontinele <contato@fontinele.dev>
2026-04-23 17:35:01 -0300

    feat: implements CI and Vitest

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -4,7 +4,7 @@ import { User } from "../entities/User";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: "database.sqlite",
+  database: process.env.NODE_ENV === "test" ? ":memory:" : "database.sqlite",
   synchronize: true,
   logging: false,
   entities: [User]
